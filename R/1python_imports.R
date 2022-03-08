df875f647cad16e61c7c0e7ee11a1ba5c07e90b6
Filipe Pinto Reis <freis@semasio.com>
2022-03-08 03:44:32 +0000

    All files within the python folder are run

---
+++ R/1python_imports.R
@@ -0,0 +1,7 @@
+# This file imports all python functions required by this package
+# The file name starts with a 1 so that it is the first in alphabetical order.
+# This way, the python functions are loaded to memory before any other script runs.
+
+for (file_name in list.files(paste('inst','python', sep = .Platform$file.sep))){
+    source_python(paste('inst','python', file_name, sep = .Platform$file.sep))
+}

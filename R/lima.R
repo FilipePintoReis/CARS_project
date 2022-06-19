f9b4a8655e634a583507d0be8f69765823b7c29d
Filipe Pinto Reis <freis@semasio.com>
2022-06-19 20:34:21 +0100

    Moved cp to a better location

--- R/lima.R
+++ R/lima.R
@@ -1,44 +1,3 @@
-#' Candidates' Color Priority
-#'
-#' @description Classification of candidates according to waiting list
-#' time on dialysis' quartiles and two cPRA cutoff values.
-#' @param data A data frame with information for candidates' waiting list.
-#' @param q2 A numerical value for the median of candidates' waiting list.
-#' @param q3 A numerical value for the third quartile of candidates' waiting list.
-#' @param cPRA1 A numerical value (0-100) for the lower cPRA cutoff.
-#' @param cPRA2 A numerical value (0-100) for the higher cPRA cutoff. cPRA2
-#' must be greater than cPRA1.
-#' @return A data frame with a new column 'cp' (color priority)
-#' @examples
-#' cp(data = candidates, q2 = 60, q3 = 100, cPRA1 = 50, cPRA2 = 85)
-#' @export
-cp <- function(data = candidates,
-               q2 = 60,
-               q3 = 100,
-               cPRA1 = 50,
-               cPRA2 = 85){
-
-  # verify function parameters
-  if(cPRA2 < cPRA1){
-    stop("higher cPRA cutoff value (cPRA2) must be greater than lower cPRA cutoff (cPRA1)!\n")
-  } else if (q2 >= q3){
-    stop("median time on dialysis quartiles must be lower than third quartile: q2 < q3!\n")
-  }
-
-  data <- data %>%
-    dplyr::mutate(cp = ifelse(urgent == 1, 1,
-                            ifelse(cPRA >= cPRA2 | dialysis >= q3, 2,
-                                   ifelse(cPRA >= cPRA1 | dialysis >= q2, 3, 4)
-                                   )
-                            ),
-                  cp = factor(cp, levels = 1:4,
-                              labels = c('Red', 'Orange', 'Yellow', 'Green')
-                              )
-                  )
-
-  return(data)
-}
-
 #' Candidates' ordering according to Lima's algorithm 'USING data.table'
 #'
 #' @description Ordering of waitlisted candidates for a given donor and
@@ -58,6 +17,7 @@ cp <- function(data = candidates,
 #' @param q3 A numerical value for the third quartile of candidates' waiting list.
 #' @param cPRA1 A numerical value (0-100) for the lower cPRA cutoff.
 #' @param cPRA2 A numerical value (0-100) for the higher cPRA cutoff. cPRA2
+#' @param check.validity Logical to decide whether to validate input.
 #' @return An ordered data frame with a column 'cp' (color priority),
 #' 'sp', 'hi' and 'mmHLA'.
 #' @examples
@@ -76,7 +36,12 @@ lima <- function(iso = TRUE
                   , q2 = 60
                   , q3 = 100
                   , cPRA1 = 50
-                  , cPRA2 = 85){
+                  , cPRA2 = 85
+                  , check.validity = TRUE){
+  
+  if(check.validity){
+
+  }
 
   n <- max(1, n)
 
--- R/utility.R
+++ R/utility.R
@@ -1,3 +1,5 @@
+################# Field validation functions #################
+
 #' Tests if element is a valid Blood Group character
 #' @param input_string A character from env$valid.blood.groups
 #' @noRd
@@ -84,6 +86,10 @@ cPRA_checker <- function(input_number){
     stop("Invalid age: cPRA should be lower or equal to ", env$cPRA.maximum, ".")
   }
 }
+
+################# Field validation functions #################
+
+################# File / Dataframe validation functions #################
 
 #' Validates the Candid file.
 #' Makes sure the header matches the header that a candid file should have.
@@ -210,3 +216,52 @@ validate_candid_uk <- function(file_name, file_type, csv_separator = ";"){
   uk_candidate_dataframe_check(file)
 }
 
+################# File / Dataframe validation functions #################
+
+################# Algorithm helper functions #################
+
+#' Candidates' Color Priority
+#'
+#' @description Classification of candidates according to waiting list
+#' time on dialysis' quartiles and two cPRA cutoff values.
+#' @param data A data frame with information for candidates' waiting list.
+#' @param q2 A numerical value for the median of candidates' waiting list.
+#' @param q3 A numerical value for the third quartile of candidates' waiting list.
+#' @param cPRA1 A numerical value (0-100) for the lower cPRA cutoff.
+#' @param cPRA2 A numerical value (0-100) for the higher cPRA cutoff. cPRA2
+#' must be greater than cPRA1.
+#' @param check.validity Logical to decide whether to validate input.
+#' @return A data frame with a new column 'cp' (color priority)
+#' @examples
+#' cp(data = candidates, q2 = 60, q3 = 100, cPRA1 = 50, cPRA2 = 85)
+#' @export
+cp <- function(data = candidates,
+               q2 = 60,
+               q3 = 100,
+               cPRA1 = 50,
+               cPRA2 = 85,
+               check.validity = TRUE){
+
+  if(check.validity){
+    if(cPRA2 < cPRA1){
+      stop("higher cPRA cutoff value (cPRA2) must be greater than lower cPRA cutoff (cPRA1)!\n")
+    } else if (q2 >= q3){
+      stop("median time on dialysis quartiles must be lower than third quartile: q2 < q3!\n")
+    }
+  }
+
+  data <- data %>%
+    dplyr::mutate(cp = ifelse(urgent == 1, 1,
+                            ifelse(cPRA >= cPRA2 | dialysis >= q3, 2,
+                                   ifelse(cPRA >= cPRA1 | dialysis >= q2, 3, 4)
+                                   )
+                            ),
+                  cp = factor(cp, levels = 1:4,
+                              labels = c('Red', 'Orange', 'Yellow', 'Green')
+                              )
+                  )
+
+  return(data)
+}
+
+################# Algorithm helper functions #################
